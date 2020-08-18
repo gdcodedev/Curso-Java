8f7484dd9cab71c771852c29874b526df9e35e51
Guilherme Cardoso <guilhermecardoso@uni9.edu.br>
2020-08-18 15:59:17 -0300

    Conversão de números para String.

---
+++ exercícios/src/fundamentos/ConversaoNumeroString.java
@@ -0,0 +1,25 @@
+package fundamentos;
+
+public class ConversaoNumeroString {
+
+    public static void main(String[] args) {
+        //Conversão número para String
+        Integer num1 = 10000;
+        System.out.println(num1.toString().length());
+
+        int num2 = 100000;
+        System.out.println(Integer.toString(num2).length());
+
+        System.out.println("" + num1);
+        System.out.println("" + num2);
+
+
+
+
+
+
+    
+
+    }
+}
+
