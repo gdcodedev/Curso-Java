a2a8cf284f2f90c08dab165639f8efedd277afb3
Guilherme Cardoso <guilhermecardoso@uni9.edu.br>
2020-08-18 16:43:10 -0300

    Conversão de números para String.

---
+++ exercícios/src/fundamentos/ConversaoStringNumero.java
@@ -0,0 +1,28 @@
+package fundamentos;
+
+import javax.swing.*;
+
+public class ConversaoStringNumero {
+                //Conversão String em número e parse double.
+    public static void main(String[] args) {
+
+        String valor1 = JOptionPane.showInputDialog("" +
+                "Digite o primeiro número.");
+
+        String valor2 = JOptionPane.showInputDialog("" +
+                "Digite o segundo número.");
+
+        System.out.println(valor1 + valor2);
+
+        double numero1 = Double.parseDouble(valor1);
+        double numero2 = Double.parseDouble(valor2);
+
+        double soma = numero1 + numero2;
+        System.out.println("Soma é " + soma);
+        System.out.println("Média é " + soma / 2);
+
+
+
+
+    }
+}
