67a7286a6830dd16f176be5c12e7bfa2c11c7da7
Guilherme Cardoso <guilhermecardoso@uni9.edu.br>
2020-08-18 13:41:12 -0300

    Adicionando exercicios ambiente trabalho

---
+++ exercícios/src/fundamentos/TipoString.java
@@ -0,0 +1,41 @@
+package fundamentos;
+
+public class TipoString {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Olá pessoa".charAt(2));
+		
+		String s = "Boa tarde";
+		System.out.println(s.concat("!!!"));
+		System.out.println(s + "!!!");
+		System.out.println(s.startsWith("Boa"));
+		System.out.println(s.toLowerCase().startsWith("boa"));
+		System.out.println(s.endsWith("tarde"));
+		System.out.println(s.length());
+	    System.out.println(s.toLowerCase().equals("boa tarde"));
+	    System.out.println(s.equalsIgnoreCase("boa tarde"));
+		
+	    var nome = "Pedro";
+	    var sobrenome = "Cardoso";
+	    var idade = 33;
+	    var salario = 12345.987;
+	    
+	    System.out.println("Nome: " + nome + " \nSobrenome: " + sobrenome + "\nidade: " + idade + "\nsalario:" + salario + "\n\n");
+	    
+	    
+	    System.out.printf("O senhor %s %s tem %d anos e ganha R$ %.2f", nome, sobrenome, idade, salario);
+	    
+	    String frase = String.format("\nO senhor %s %s tem %d anos e ganha R$ %.2f", nome, sobrenome, idade, salario);
+	    System.out.println(frase);
+
+	    
+	
+	    
+
+		
+		
+		
+		
+	}
+}
