6bdb65bcd1725c7dc776fcadfe0aa80408569131
Guilherme Cardoso <guilhermecardoso@uni9.edu.br>
2020-08-18 14:30:09 -0300

    Conversão de tipos Primitivos Numericos.

---
+++ exercícios/src/fundamentos/ConversaoTiposPrimitivosNumericos.java
@@ -0,0 +1,27 @@
+package fundamentos;
+
+import com.sun.security.auth.module.UnixSystem;
+
+public class ConversaoTiposPrimitivosNumericos {
+
+    public static void main(String[] args) {
+
+        double a = 1; // Conversão implicita
+        System.out.println(a);
+
+        float b = (float) 1.12345; // Conversão explícita (CAST)
+        System.out.println(b);
+
+        int c = 4;  // Conversão explícita (CAST)
+        byte d = (byte) c;
+        System.out.println(d);
+
+        double e = 1.0; // Conversão explícita (CAST)
+        int f = (int) e;
+        System.out.print(f);
+
+
+
+
+    }
+}
