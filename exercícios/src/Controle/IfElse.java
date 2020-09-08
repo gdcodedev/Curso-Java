bf099b6ee7d0aa7a4b0f4c58e63d08f6ceff2532
Guilherme Cardoso <guilhermecardoso@uni9.edu.br>
2020-09-08 14:53:06 -0300

    Exercicio IF/ELSE

---
+++ exercícios/src/Controle/IfElse.java
@@ -0,0 +1,22 @@
+package Controle;
+
+import javax.swing.*;
+
+public class IfElse {
+
+    public static void main(String[] args) {
+
+        String valor = JOptionPane.showInputDialog(
+                "Informe o número");
+
+        int numero = Integer.parseInt(valor);
+
+
+            if(numero % 2 == 0) {
+                System.out.println("Numero par!");
+            } else {
+                System.out.println("Número impar!");
+            }
+    }
+
+}
