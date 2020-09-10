f8c6e0a78f924963f78e5556cdcfb9e62253e974
Guilherme <guilhermecardoso@uni9.edu.br>
2020-09-10 10:23:55 -0300

    WhileDeterminado

---
+++ exercícios/src/Controle/For1.java
@@ -0,0 +1,10 @@
+package Controle;
+
+public class For1 {
+    public static void main(String[] args) {
+        for (int contador = 0; contador <= 20; contador += 2){
+            System.out.println("Bom dia");
+        }
+
+    }
+}
