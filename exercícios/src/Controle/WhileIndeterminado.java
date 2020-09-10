cca93c6fccc55ef305c241b8ac791365c4ffe60e
Guilherme <guilhermecardoso@uni9.edu.br>
2020-09-10 10:38:58 -0300

    // While para quantidade indeterminada;

---
+++ exercícios/src/Controle/WhileIndeterminado.java
@@ -0,0 +1,25 @@
+package Controle;
+
+import java.util.Scanner;
+
+public class WhileIndeterminado {
+
+    // While para quantidade indeterminada;
+
+    public static void main(String[] args) {
+
+        Scanner entrada =  new Scanner(System.in);
+
+        String valor = "";
+        while (!valor.equalsIgnoreCase("sair")){
+            System.out.print("Você diz : ");
+            valor = entrada.nextLine();
+
+
+        }
+
+
+        entrada.close();
+
+    }
+}
