9f12546174861bd04407ac9cacaa1d2790bc1031
Guilherme <guilhermecardoso@uni9.edu.br>
2020-09-17 12:00:13 -0300

    Exercicio For

---
+++ exercícios/src/Controle/For3.java
@@ -0,0 +1,14 @@
+package Controle;
+
+public class For3 {
+
+    public static void main(String[] args) {
+
+        for (  int i = 0; i < 10; i++){
+            System.out.println(i);
+        }
+    }
+
+}
+
+
