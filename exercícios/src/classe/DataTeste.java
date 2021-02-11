4c128d288d4ea656376f180975b2d214ee96e276
Guilherme Cardoso <guilhermecardoso@uni9.edu.br>
2021-02-11 01:20:35 -0300

    Conceitos Classe Objeto

---
+++ exercícios/src/classe/DataTeste.java
@@ -0,0 +1,22 @@
+package classe;
+
+public class DataTeste {
+
+    public static void main(String[] args) {
+
+            Data Datadia = new Data();
+            Datadia.dia = 21;
+            Datadia.mes = 07;
+            Datadia.ano = 1991;
+
+        System.out.printf("%d/%d/%d", Datadia.dia, Datadia.mes, Datadia.ano);
+
+
+
+
+
+
+
+
+    }
+}
