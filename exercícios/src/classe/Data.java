7080c9e7cfbfe612a80263b21c6d008eae7ee98d
Guilherme Cardoso <guilhermecardoso@uni9.edu.br>
2021-02-12 01:41:17 -0300

    Refactor

--- exercícios/src/classe/Data.java
+++ exercícios/src/classe/Data.java
@@ -5,5 +5,11 @@ public class Data {
         int dia;
         int mes;
         int ano;
+
+        String obterDataFormatada(){
+                return String.format("%d/%d/%d", dia, mes, ano);
+
+        }
+
 }
 
--- exercícios/src/classe/DataTeste.java
+++ exercícios/src/classe/DataTeste.java
@@ -8,8 +8,9 @@ public class DataTeste {
             Datadia.dia = 21;
             Datadia.mes = 07;
             Datadia.ano = 1991;
+    //refactor
+        System.out.println(Datadia.obterDataFormatada());
 
-        System.out.printf("%d/%d/%d", Datadia.dia, Datadia.mes, Datadia.ano);
 
 
 
