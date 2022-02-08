9d8bf0839d562b4e3c6a982b9ba2cb43ec76b25a
Guilherme Cardoso <65917790+gdcodedev@users.noreply.github.com>
2022-02-08 11:35:08 -0300

    update
    update

---
+++ Métodos/src/metodos.java
@@ -0,0 +1,17 @@
+public class metodos {
+
+
+    // Outros atributos
+
+    private final double  totalValue;
+
+    // Outros métodos
+
+    public double calculateFee() {
+      if (this.totalValue  > 100 ) {
+        return this.totalValue * 0.99;
+      } else {
+        return this.totalValue;
+      }
+    }
+  }
